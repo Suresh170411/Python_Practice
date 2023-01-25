af1c9e4d95104f6d1fa3e3932b7ac3c7e700e664
suresh170411 <suresh.20fk@gmail.com>
2023-01-25 08:47:45 +0530

    Python practice string operations

--- Practice1.py
+++ Practice1.py
@@ -4,4 +4,4 @@ name = "Suresh"
 print("Age is : " +str(age))
 print("Name is : "+name)
 print("Name is : "+name + "Age is : "+ str(age)) #string concatenation
-print(name.upper())
+print("Name is : "+name.upper())
