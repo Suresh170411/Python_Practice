53beb878d493dd0e7dba7a17d9f2f99b44300ced
suresh170411 <suresh.20fk@gmail.com>
2023-01-25 09:02:32 +0530

    Python practice if else

---
+++ Practice2.py
@@ -0,0 +1,8 @@
+age = 15
+
+if age >= 18:
+    print("You are an adult and you are eligible for vote")
+elif age >=60:
+    print("You are a senior citizen")
+else:
+    print("You are minor and you can't vote")
